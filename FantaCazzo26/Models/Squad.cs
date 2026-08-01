000dad557a1c63f74653da64ea62478eef93533f
federico <federicofalqui@gmail.com>
2026-08-01 15:51:31 +0200

    aa

--- FantaCazzo26/Models/Squad.cs
+++ FantaCazzo26/Models/Squad.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FantaCazzo26.Models;
 
+[Table("squads")]
 public class Squad
 {
-    
+    [Key]
+    public long Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string President { get; set; } = string.Empty;
+
+    public int NumGk { get; set; }
+
+    public int NumDef { get; set; }
+
+    public int NumMid { get; set; }
+
+    public int NumStr { get; set; }
+
+    public int Credits { get; set; }
 }
